767c6f595a49c2155c26bbcb1bcf8c67e664f711
MarshmallowKA24 <alejandrolopez3131@gmail.com>
2025-06-06 17:24:52 +0300

    First commit of Turkish Spanish project

---
+++ js/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configuración correcta de EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Public_html'));
+
+// ¡Esto es crucial! Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'Public_html')));
+
+// Rutas
+app.get('/', (req, res) => res.render('index'));
+app.get('/gramatica', (req, res) => res.render('gramatica'));
+app.get('/recursos', (req, res) => res.render('recursos'));
+app.get('/juegos', (req, res) => res.render('juegos'));
+
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+});
